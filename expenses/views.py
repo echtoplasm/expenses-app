dc6d8af164456eb1719706da808c10f40622d697
echtoplasm <zacharyamassey@gmail.com>
2025-02-17 11:50:06 -0500

    Added form for expense submission, updated the view for form view, updated template for form

--- expenses/views.py
+++ expenses/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Expense
+from .forms import ExpenseForm
 # Create your views here.
 
 def expense_list(request):
     expenses = Expense.objects.all()
     total_expense = sum(exp.amount for exp in expenses)
-    return render(request, 'expenses/expense_list.html', {'expenses': expenses, 'total_expense': total_expense})
+
+
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('expense_list')
+    else:
+        form = ExpenseForm()
+
+    return render(request, 'expenses/expense_list.html', {
+        'expenses': expenses,
+        'total_expense': total_expense,
+        'form': form
+    })
+
