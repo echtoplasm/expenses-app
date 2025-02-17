2749da1995a95848bfb160bc07a78c0e209b8f34
echtoplasm <zacharyamassey@gmail.com>
2025-02-17 11:08:05 -0500

    added expense model to expenses/models.py

--- expenses/models.py
+++ expenses/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Expense(models.Model):
+    CATEGORY_CHOICES = [
+        ('ONLINE_SERVICES', 'Online Services'),
+        ('TRAVEL', 'Travel'),
+        ('FOOD', 'Food'),
+        ('RENT', 'Rent'),
+        ('OTHER', 'Other'),
+    ]
 
+    name = models.CharField(max_length=200)
+    category = models.CharField(max_length=200, choices=CATEGORY_CHOICES)
+    amount = models.DecimalField(max_digits=100, decimal_places=2)
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.name} - {self.amount} - {self.date}"
 
     
