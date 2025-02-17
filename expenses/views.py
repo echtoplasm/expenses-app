6f29ada76cf2ca3d9570b7ca512c73ff1dbcef12
echtoplasm <zacharyamassey@gmail.com>
2025-02-17 11:15:59 -0500

    added expense_list view, made superuser, and added expenses in admin panel

--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Expense
 # Create your views here.
+
+def expense_list(request):
+    expenses = Expense.objects.all()
+    total_expense = sum(exp.amount for exp in expenses)
+    return render(request, 'expenses/expense_list.html', {'expenses': expenses, 'total_expense': total_expense})
